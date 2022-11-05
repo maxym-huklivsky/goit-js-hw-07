140dd3d7a017065415a7484f5a2dadc8b188a727
maxym-huklivsky <106371809+maxym-huklivsky@users.noreply.github.com>
2022-11-05 16:00:50 +0200

    task 1 is completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,9 +6,14 @@ const galleryEl = document.querySelector(".gallery");
 const markup = galleryItems
   .map(({ preview, original, description }) => {
     return `
-  <div class="gallery__item">
+    <div class="gallery__item">
     <a class="gallery__link" href="${original}">
-     <img class="gallery__image" src="${preview}" alt="${description}" />
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
     </a>
   </div>
   `;
@@ -17,6 +22,30 @@ const markup = galleryItems
 
 galleryEl.insertAdjacentHTML("beforeend", markup);
 
-galleryEl.addEventListener("click", onGetIncreasedPic);
+galleryEl.addEventListener("click", onOpenIncreasedPic);
 
-function onGetIncreasedPic(e) {}
+function onOpenIncreasedPic(e) {
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" alt='${e.target.alt}'>`
+  );
+
+  instance.show();
+
+  document.addEventListener(
+    "keydown",
+    (e) => {
+      if (e.code !== "Escape") {
+        return;
+      }
+
+      instance.close();
+    },
+    { once: true }
+  );
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+	<a class="gallery__item" href="${original} data-lightbox='lbox'">
+		<img
+			class="gallery__image"
+			src="${preview}"
+			alt="${description}"
+	 	/>
+  	</a>
+  `;
+  })
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+galleryEl.addEventListener("click", onOpenIncreasedPic);
+
+function onOpenIncreasedPic(e) {
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  e.preventDefault();
+}
